6837621ce1ae8a07b5ed9b26d9a3f62ada3235c3
aungkaungmyat <115795473+aungkaungmyat9504@users.noreply.github.com>
2023-08-08 09:38:48 +0630

    Changed Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -65,7 +65,7 @@ const Footer = () => {
           </div>
         </div>
       </div>
-      <p className="text-center py-4">Powered by Coin Gecko</p>
+      <p className="text-center py-4">Developed by Aung Kaung Myat</p>
     </div>
   );
 };
